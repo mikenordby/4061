56bfe9021b188a1554b87ed6b956d784d058e8c1
Logan Stucker <stuck124@umn.edu>
2017-10-07 22:07:09 -0500

    fix# Please enter the commit message for your changes. Lines starting

--- hw2.c
+++ hw2.c
@@ -26,6 +26,10 @@
 #define NAMESIZE 256
 #define TOKENSIZE 100
 
+#define NORMAL_COLOR  "\x1B[0m"
+#define GREEN  "\x1B[32m"
+#define BLUE  "\x1B[34m"
+
 int main(int argc, char *argv[])
 {
 	char *dirname;
@@ -99,11 +103,40 @@ int main(int argc, char *argv[])
     }
 
     printf("The total size of the files in %s is : %d bytes\n",dirname,totalsum);
-	
+	recsearch(input_dir_name);
 	}
 
 	else if(choice == 2){
 		printf("\nEXECUTING \"2. List all zero length files in a directory\"\n");
+		
+			
+	    /* Check if the directory entered exists or not*/
+    stat(input_dir_name,&statbuf);
+    if(!(S_ISDIR(statbuf.st_mode))){
+        printf("The directory name is not valid. Directory does not exist\n");
+        exit(0);
+    }
+
+    if((dp=opendir(input_dir_name))==NULL){
+        perror("Error while opening the directory");
+        exit(0);
+    }
+    /* Loop through the directory structure */
+    chdir(input_dir_name); //previously missing
+    while( (direntry = readdir(dp)) != NULL )
+    {
+	stat(direntry->d_name,&statbuf);
+	if(!(S_ISDIR(statbuf.st_mode)))
+	{
+		if(((int) statbuf.st_size) == 0)
+		{
+			
+	    printf("The size of file %s is :%d bytes\n",direntry->d_name,(int) statbuf.st_size);
+
+	}
+    }
+
+	}
 		
 	}
 
@@ -132,3 +165,50 @@ int main(int argc, char *argv[])
 	free(dirpath);
 	return 0;
 }
+
+void recsearch (char * path) {
+	
+	 DIR * dpp = opendir(path); // open the path
+  if(dpp==NULL) return; // if was not able return
+  struct dirent * dir; // for the directory entries
+	
+	while ((dir = readdir(dpp)) != NULL) {
+		
+		 if(dir-> d_type != DT_DIR) {
+			 
+			printf("Reg: %s\n",dir->d_name);
+		
+		}
+		 else
+      if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) // if it is a directory
+      {
+        printf("Dir: %s\n",dir->d_name); // print its name in green
+        char d_path[255]; // here I am using sprintf which is safer than strcat
+        sprintf(d_path, "%s/%s", path, dir->d_name);
+        recsearch(d_path); // recall with the new path
+      }
+    }
+    closedir(dpp); // finally close the directory
+	
+	}
+	
+
+
+	
+// 	stat(dirnam,&statbuf);
+//	 /* Loop through the directory structure */
+ //   chdir(dirnam); //previously missing
+//    while( (direntry = readdir(dp)) != NULL )
+//    {
+//	stat(direntry->d_name,&statbuf);
+	
+//	if (S_ISDIR(statbuf.st_mode)) {
+//		printf("Dir: %s\n",direntry->d_name);
+		//recsearch(direntry);
+//    }
+     
+//    if (S_ISREG(statbuf.st_mode)) {
+//	printf("Reg: %s\n",direntry->d_name);
+//      }
+	
+//}
